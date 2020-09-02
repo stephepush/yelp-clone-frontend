bce07a7a97c19f4c9ea6ef8a76b8cb6f10cc2719
stephepush <7speter@gmail.com>
2020-09-02 15:26:26 -0400

    Adds axios, cors/ Works on create a restaurant routing and rendering

---
+++ src/components/RestaurantList.jsx
@@ -0,0 +1,60 @@
+import React, {useEffect, useContext } from 'react';
+import RestaurantFinder from "../apis/RestaurantFinder";
+import { RestaurantsContext } from '../context/RestaurantContext';
+
+const RestaurantList = () => {
+    const {restaurants, setRestaurants} = useContext(RestaurantsContext)
+    useEffect(() => {
+        const fetchData = async () => {
+           try {
+            const response = await RestaurantFinder.get("/");
+            setRestaurants(response.data.data.restaurants)
+            console.log(response);
+        } catch (err) {
+
+        } 
+        }
+        fetchData();
+    },[])
+    return (
+        <div className="list-group">
+            <table className="table table-hover table-dark">
+                <thead>
+                   <tr className="bg-primary">
+                       <th scope="col">Restaurant</th>
+                       <th scope="col">Location</th>
+                       <th scope="col">Price Range</th>
+                       <th scope="col">Rating</th>
+                       <th scope="col">Edit</th> 
+                       <th scope="col">Delete</th>
+                    </tr> 
+                </thead>
+                <tbody>
+                    {restaurants && restaurants.map((restaurant) => {
+                        return(
+                            <tr key={restaurant.id}>
+                                <td>{restaurant.name}</td>
+                                <td>{restaurant.location}</td>
+                                <td>{"$".repeat(restaurant.price_range)}</td>
+                                <td>reviews</td>
+                                <td><button className="btn btn-warning">Update</button></td>
+                                <td><button className="btn btn-primary">Delete</button></td>
+                            </tr>
+                        )
+                        
+                    })}
+                    {/* <tr>
+                        <td>Macdonalds</td>
+                        <td>New York</td>
+                        <td>$$</td>
+                        <td>Rating</td>
+                        <td><button className="btn btn-warning">Update</button></td>
+                        <td><button className="btn btn-primary">Delete</button></td>
+                    </tr> */}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default RestaurantList
---
+++ src/routes/RestaurantDetailPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RestaurantDetailPage = () => {
+    return (
+        <div>
+            Deets
+        </div>
+    )
+}
+
+export default RestaurantDetailPage;
